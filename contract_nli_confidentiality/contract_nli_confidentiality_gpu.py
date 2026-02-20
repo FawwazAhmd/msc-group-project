a537a64726c38675bb8d4ea53998486e1076e94b
Sinan Mohammed <msinan1001@gmail.com>
2026-02-20 00:20:34 +0000

    minor changes

--- contract_nli_confidentiality/contract_nli_confidentiality_gpu.py
+++ contract_nli_confidentiality/contract_nli_confidentiality_gpu.py
@@ -2,7 +2,7 @@ import pandas as pd
 import torch
 import os
 from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline
-from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
+from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score, confusion_matrix
 from tqdm import tqdm
 
 # =====================================
@@ -131,9 +131,13 @@ accuracy = accuracy_score(df[LABEL_COLUMN], df["prediction"])
 f1 = f1_score(df[LABEL_COLUMN], df["prediction"])
 precision = precision_score(df[LABEL_COLUMN], df["prediction"])
 recall = recall_score(df[LABEL_COLUMN], df["prediction"])
+cm = confusion_matrix(df[LABEL_COLUMN], df["phi3"])
 
 print("\nEvaluation Results:")
 print("Accuracy:", round(accuracy, 4))
 print("F1 Score:", round(f1, 4))
 print("Precision:", round(precision, 4))
 print("Recall:", round(recall, 4))
+
+print("\nConfusion Matrix:")
+print(cm)
